fc87620c1dd85eb754f6aaca956f680bdecdf457
lzzeng <ul.wyc.khn@qq.com>
2019-06-09 17:39:22 +0800

    update redis cluster playbook

--- roles_redis_cluster_make/roles/redis/filter_plugins/collections.py
+++
@@ -1,9 +0,0 @@
-def arraypermute(collection, key):
-    return [ str(i) + str(j) for i in collection for j in key ]
-
-
-class FilterModule(object):
-    def filters(self):
-        return {
-            'arraypermute': arraypermute
-        }
